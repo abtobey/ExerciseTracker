c1baf79eb8a7b51732ce8f870d163b324895c49e
Aaron Tobey <abtobey@gmail.com>
2020-09-11 13:17:16 -0500

    deploying to heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3005;
 
 const db = require("./models");
 const { Exercise, Workout } = require("./models");
@@ -17,7 +17,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useFindAndModify: false});
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
+{ 
+  
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  });
 
 app.post("/api/workouts", (req,res) => {
   const newWorkout= new Workout(req.body);
